a33ce16842fd42cf931205c39e0e88963749e04b
mlmpk <mlempik@gmail.com>
2014-06-04 10:38:06 +0200

    made some changes...not working for 100%

--- src/de/lempikbubar/src/blatt08/EditDistance.java
+++ src/de/lempikbubar/src/blatt08/EditDistance.java
@@ -6,41 +6,57 @@ import java.util.ArrayList;
 public class EditDistance {
 
 	public static boolean extendedOutput = false;
+	public static char[] tmp;
 	public static String REPLACE = "Ersetze";
 	public static String INSERT = "Fuege";
 	public static String DELETE = "Loesche";
+	public static int[][] m;
 
 	public static int distance(String a, String b) {
 		int matrix[][] = new int[a.length() + 1][b.length() + 1];
+		
 		// Matrix initialisieren
-		for (int i = 0; i < a.length() + 1; i++) {
-			matrix[i][0] = i;
-		}
 		for (int i = 0; i < b.length() + 1; i++) {
 			matrix[0][i] = i;
 		}
 		
 		
 		for (int i = 1; i < a.length() + 1; i++) {
+			matrix[i][0] = i;
 			for (int j = 1; j < b.length() + 1; j++) {
 				
-				int match = (a.charAt(i - 1) == b.charAt(j - 1)) ? 0 : 1; // wenn chars gleich sind, dann match = 1, sonst 0
+				//int match = (a.charAt(i - 1) == b.charAt(j - 1)) ? 0 : 1; // wenn chars gleich sind, dann match = 1, sonst 0
 				
-				int mini = matrix[i - 1][j] + 1;
+//				int mini = matrix[i - 1][j] + 1;
+//				
+//				if (matrix[i][j - 1] + 1 < mini) {
+//					mini = matrix[i][j - 1] + 1;
+//				}
+//				
+//				if (matrix[i - 1][j - 1] + match < mini) {
+//					mini = matrix[i - 1][j - 1] + match;
+//				}
+//				matrix[i][j] = mini;
 				
-				if (matrix[i][j - 1] + 1 < mini) {
-					mini = matrix[i][j - 1] + 1;
-				}
+					matrix[i][j] = matrix[i-1][j-1];
+					if(a.charAt(i-1) != (b.charAt(j-1))){
+						matrix[i][j]++;
+					}
+					if(matrix[i][j] > matrix[i-1][j] +1){
+						matrix[i][j] = matrix[i-1][j] +1;
+					}
+					if(matrix[i][j] > matrix[i][j-1] +1){
+						matrix[i][j] = matrix[i][j-1] +1;
+					}
 				
-				if (matrix[i - 1][j - 1] + match < mini) {
-					mini = matrix[i - 1][j - 1] + match;
-				}
-				matrix[i][j] = mini;
 			}
 			
-			printMatrix(matrix, a, b);
+//			printMatrix(matrix, a, b);
 			
 		}
+		
+		printMatrix(matrix, a, b);
+		m = matrix;
 		
 		return matrix[a.length()][b.length()];
 
@@ -68,83 +84,32 @@ public class EditDistance {
 		
 	}
 
-	/*public static int distance(String a, String b) {
-
-		int lengthStringA = a.length() + 1;
-		int lengthStringB = b.length() + 1;
-
-		// the array of distances
-		int[] cost = new int[lengthStringA];
-		int[] newcost = new int[lengthStringA];
-
-		// initial cost of skipping prefix in String s0
-		for (int i = 0; i < lengthStringA; i++) {
-			cost[i] = i;
-		}
-		// dynamicaly computing the array of distances
-
-		// transformation cost for each letter in s1
-		for (int j = 1; j < lengthStringB; j++) {
-
-			// initial cost of skipping prefix in String s1
-			newcost[0] = j - 1;
-
-			// transformation cost for each letter in s0
-			for (int i = 1; i < lengthStringA; i++) {
-
-				// matching current letters in both strings
-				int match = (a.charAt(i - 1) == b.charAt(j - 1)) ? 0 : 1;
-
-				// computing cost for each transformation
-				int cost_replace = cost[i - 1] + match;
-				int cost_insert = cost[i] + 1;
-				int cost_delete = newcost[i - 1] + 1;
-
-				if (extendedOutput) {
-					int minInsertDelete = Integer.MAX_VALUE;
-					if (cost_insert <= cost_delete) {
-						minInsertDelete = cost_insert;
-						printEditOperations(INSERT, match);
-					} else {
-						minInsertDelete = cost_delete;
-						printEditOperations(DELETE, match);
-					}
-
-					int minInsertDeleteReplace = Integer.MAX_VALUE;
-
-					if (minInsertDelete <= cost_replace) {
-						minInsertDeleteReplace = minInsertDelete;
-					} else {
-						minInsertDeleteReplace = cost_replace;
-						printEditOperations(REPLACE, match);
-					}
-
-					newcost[i] = minInsertDeleteReplace;
-
-				} else {
-					// keep minimum cost
-					newcost[i] = Math.min(Math.min(cost_insert, cost_delete),
-							cost_replace);
-				}
-			}
-
-			// swap cost/newcost arrays
-			int[] swap = cost;
-			cost = newcost;
-			newcost = swap;
-		}
-
-		// the distance is the cost for transforming all letters in both strings
-		return cost[lengthStringA - 1];
-
-	}
-	
-	*/
-
-	private static void printEditOperations(String operation, int cost) {
-
-		System.out.println("Kosten " + cost + ": +" + operation + " ");
-
+	public static void printEditOperations(int d[][], int i, int j, char[] y){
+  		if (i>0 && d[i-1][j] + 1 == d[i][j]){
+   			printEditOperations(d,i-1, j, y);
+    		tmp[j] = y[j];
+    		System.out.println("füge " + y[j] + " an Position " + j+1 + " ein       Kosten: 1 ---> " + new String(tmp));
+  		}
+  		if (j>0 && d[i][j-1] + 1 == d[i][j]){
+      		printEditOperations(d,i, j-1, y);
+      		int n = j+1;
+      		while(tmp[n] != ' ' && n < tmp.length-1){
+        		tmp[n-1] = tmp[n];
+        		n++;
+      		}
+      		tmp[n] = ' ';
+      		//System.out.println(" Löschen        Kosten: 1 "+ new String(tmp));
+  		}
+ 		if (i>0 && j>0 && d[i-1][j-1] + 1 == d[i][j]){
+     	 	printEditOperations(d,i-1, j-1, y);
+      		System.out.print(tmp[j-1]+" Ersetzen durch "+  y[i-1]+"      Kosten: 1 ");
+      		tmp[j-1] = y[i-1];
+     		System.out.println("--->" + new String(tmp));
+  		}
+  		if (i>0 && j>0 && d[i-1][j-1]  == d[i][j]){
+   	 	  	printEditOperations(d,i-1, j-1, y);
+   			//System.out.println(tmp[j-1] +" "+ "Bleibt an der stelle" + j + "  Kosten: nix ---> " + new String(tmp));
+  		}
 	}
 
 	public static void main(String[] args) {
@@ -176,6 +141,7 @@ public class EditDistance {
 				extendedOutput = true;
 
 				getDistancesForTwoStrings(args);
+				
 			} else {
 				System.out.println(Statics.SYNTAX_MSG);
 			}
@@ -190,9 +156,16 @@ public class EditDistance {
 	private static void getDistancesForTwoStrings(String[] args) {
 		String a = args[0];
 		String b = args[1];
-
-		System.out.println("Die minimale Editierdistanz von \"" + a
-				+ "\" und \"" + b + "\" ist " + distance(a, b) + ".");
+		
+		System.out.println("Loesung fuer \"" + a
+				+ "\" und \"" + b + "\" mit Gesamtkosten  " + distance(a, b) + ".");
+		
+		System.out.println("------------------------------------------------------------------");
+		if(extendedOutput){
+			tmp = a.toCharArray();
+			printEditOperations(m, m.length-1, m[0].length-1,b.toCharArray());
+		}
+		
 	}
 
 	private static void getDistancesForFile(String[] args) {
@@ -210,10 +183,7 @@ public class EditDistance {
 
 				for (int i = 0; i < lines.size(); i++) {
 					for (int j = 1; j < lines.size(); j++) {
-						if (!lines.get(i).equals(lines.get(j))) { // nur, wenn
-																	// sich die
-																	// Strings
-																	// unterscheiden
+						if (!lines.get(i).equals(lines.get(j))) { // nur, wenn sich die  Strings unterscheiden
 							String a = lines.get(i);
 							String b = lines.get(j);
 							int distance = distance(a, b);
@@ -233,6 +203,11 @@ public class EditDistance {
 									minDistance.setA(a);
 									minDistance.setB(b);
 									minDistance.setDistance(distance);
+								}
+								
+								if(extendedOutput){
+									tmp = a.toCharArray();
+									printEditOperations(m, m.length-1, m[0].length-1,b.toCharArray());
 								}
 							}
 
