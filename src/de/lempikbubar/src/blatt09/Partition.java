4ea031612b3ce3d68f1f0515632b553791a94d44
mlmpk <mlempik@192.168.1.12>
2014-06-05 18:00:50 +0200

    blatt09 done ... comments pending

--- src/de/lempikbubar/src/blatt09/Partition.java
+++ src/de/lempikbubar/src/blatt09/Partition.java
@@ -4,9 +4,37 @@ import java.util.ArrayList;
 
 public class Partition {
 
-	private static final String SYNTAX_MSG = "Syntax des Programmaufrufs: Partition [Ganzzahl 1] [Ganzzahl 2] [Ganzzahl ...n]";
+	private static final String SYNTAX_MSG = "Syntax des Programmaufrufs: Partition [Ganzzahl 1] [Ganzzahl 2] ...[Ganzzahl n]";
 
+	/**
+	 * Implementierung des Partitionierungs-Algorithmus
+	 * @param list
+	 * @return
+	 */
 	public static boolean partition(ArrayList<Integer> list){
+		int w = 0;
+		
+		for (int i = 0; i < list.size(); i++) {
+			w = w + list.get(i);
+		}
+		
+		if(w % 2 == 1) return false;
+		
+		int[][] matrix = new int[list.size()][w];
+		
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < w/2; j++) {
+				if( j == 0 ){
+					matrix[i][j] = 1;
+				}else if ( i == 0){
+					matrix[i][j] = 0;
+				}else if ( matrix[i-1][j] == 1 || (list.get(i) <= j && matrix[i-1][j-list.get(i)] == 1)){
+					matrix[i][j] = 1;
+				}else{
+					matrix[i][j] = 0;
+				}
+			}
+		}
 		
 		return true;
 	}
@@ -21,18 +49,16 @@ public class Partition {
 					list.add(Integer.valueOf(args[i]));
 				}
 				
-				System.out.println(partition(list));
+				System.out.println( partition(list) );
 				
 			} catch (NumberFormatException e) {
 				System.out.println(SYNTAX_MSG);
+				System.out.println(e.getLocalizedMessage());
 			}
-			
-			
 			
 		}else{
 			System.out.println(SYNTAX_MSG);
 		}
-		
 		
 	}
 	
