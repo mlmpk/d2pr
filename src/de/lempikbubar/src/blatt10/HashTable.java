c5b5e102eecb1a638bc2fab9abe63b0d85670857
mlmpk <mlempik@gmail.com>
2014-06-17 13:37:28 +0200

    blatt10 exercise 10.1 completed

--- src/de/lempikbubar/src/blatt10/HashTable.java
+++ src/de/lempikbubar/src/blatt10/HashTable.java
@@ -1,5 +1,10 @@
 package de.lempikbubar.src.blatt10;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class HashTable {
@@ -25,21 +30,22 @@ public class HashTable {
 	 */
 	public void put(String key) {
 
-		int hash = Math.abs((int) GeneralHashFunctionLibrary.RSHash(key) % hashArray.length);
+		int hash = Math.abs((int) GeneralHashFunctionLibrary.RSHash(key)
+				% hashArray.length);
 
 		HashItem item = new HashItem(key, 1);
 
 		LinkedList<HashItem> currentlist = hashArray[hash];
 
-		if (!currentlist.isEmpty()) { // Liste an der Stelle hash ist nicht // leer, daher Suche nach Vorhandensein // des Items
-		
+		if (!currentlist.isEmpty()) { // Liste an der Stelle hash ist nicht leer, daher jetzt Suche nach Vorhandensein des Items
+
 			boolean isItemInList = false;
 			for (int i = 0; i < currentlist.size(); i++) {
 				if (currentlist.get(i).getKey().equals(item.getKey())) {
 					isItemInList = true;
 					break;
 				}
-				
+
 			}
 
 			if (!isItemInList) {
@@ -52,9 +58,10 @@ public class HashTable {
 		}
 
 	}
-	
+
 	/**
 	 * Gibt HashItem zurück mit jeweiligem Key
+	 * 
 	 * @param key als String
 	 * @return HashItem
 	 */
@@ -64,42 +71,90 @@ public class HashTable {
 		int hash = (int) GeneralHashFunctionLibrary.RSHash(key) % hashArray.length;
 		// Hole die Liste am entsprechenden Hash
 		LinkedList<HashItem> currentList = hashArray[hash];
-		
+
 		if (!currentList.isEmpty()) {
-			for(int i = 0; i < currentList.size();i++){
+			for (int i = 0; i < currentList.size(); i++) {
 				// wenn der Key übereinstimmt, return das HashItem
 				if (currentList.get(i).getKey().equals(key)) {
 					return currentList.get(i);
 				}
 			}
 		}
-		
+
 		return null;
 	}
 
+	/**
+	 * Löscht alles Einträge in der Hashtable
+	 */
 	public void clear() {
 		new HashTable(hashArray.length);
 	}
 
+	/**
+	 * Gibt die Anzahl der Kollisionen zurück
+	 * 
+	 * @return Anzahl d. Kollisionen
+	 */
 	public int numberOfCollisions() {
-		
+		// wird direkt bei Einfügen berechnett und zwar, wenn der Fall
+		// vorliegt, dass bereits ein Item zum gleichen Hashwert existiert.
+		// Alternativ müsste man an dieser Stelle, über das HashArray und über alle
+		// Listen iterieren, wobei Listen mit nur einem Element nicht mitgezählt werden,
+		// da es sich dabei um keine Kollisionen handeln kann.
 		return collisions;
 	}
 
+	/**
+	 * Gibt die Hashtable auf der Konsole aus
+	 */
 	public void printHashTable() {
-		
+
 		for (int i = 0; i < hashArray.length; i++) {
 			System.out.println("Hash items with hash value " + i);
 			for (int j = 0; j < hashArray[i].size(); j++) {
 				HashItem item = hashArray[i].get(j);
-				
-				System.out.println("key: "+item.getKey() + " -- info: " + item.getInfo());
-				
+
+				System.out.println("key: " + item.getKey() + " -- info: " + item.getInfo());
+
 			}
 			System.out.println("---------------------------------------");
-			
+
 		}
-		
+
 		System.out.println("The number of collisions is " + numberOfCollisions());
 	}
+
+	/**
+	 * Die Methode liest zeilenweise aus der Datei zahlen.txt
+	 * @param Anzahl der Zeilen, die verarbeitet werden sollen
+	 * @return ArrayList vom Typ String
+	 */
+	public static ArrayList<String> readLines(int lines) {
+
+		String file = System.getProperty("user.dir") + "/vorgaben10/zahlen.txt";
+
+		ArrayList<String> list = new ArrayList<>();
+
+		try {
+			
+			BufferedReader in = new BufferedReader(new FileReader(file));
+
+			String zeile = null;
+			while ((zeile = in.readLine()) != null && list.size() <= lines) {
+				list.add(zeile);
+			}
+			in.close();
+
+		} catch (FileNotFoundException e) {
+			System.out.println("Die Datei wurde nicht gefunden! " + e.getLocalizedMessage());
+
+		} catch (IOException e) {
+			System.out.println("Es gab ein Fehler in Zusammenhang mit der Datei" + e.getLocalizedMessage());
+			e.printStackTrace();
+		}
+		return list;
+
+	}
+
 }
--- src/de/lempikbubar/src/blatt10/HashTest.java
+++ src/de/lempikbubar/src/blatt10/HashTest.java
@@ -1,27 +1,41 @@
 package de.lempikbubar.src.blatt10;
 
+import java.util.ArrayList;
+
 public class HashTest {
 
-	
 	public static void main(String[] args) {
-		HashTable tabletest = new HashTable(10);
-		
-		tabletest.put("Abgabe");
-		tabletest.put("find");
-		tabletest.put("Uhr");
-		tabletest.put("Hallo");
-		tabletest.put("Hallo");
-		tabletest.put("Hallo");
-		tabletest.put("Hallo");
-		tabletest.put("Hallo");
-		tabletest.put("Hallo");
-		tabletest.put("Hallo");
-		tabletest.put("Hallo");
-		tabletest.put("Hallo");
-		tabletest.put("Hallo");
-		
-		tabletest.printHashTable();
-		
+
+		try {
+
+			if (args.length == 2) {
+
+				int size = Integer.valueOf(args[0]);
+				int lines = Integer.valueOf(args[1]);
+				
+				if(size > 0 && lines > 0){
+					
+					HashTable ht = new HashTable(size);
+					ArrayList<String> entries = HashTable.readLines(lines);
+					
+					for (String entry : entries) {
+						ht.put(entry);
+					}
+					
+					ht.printHashTable();
+					
+				}else{
+					System.out.println("Bitte positive Ganzzahlen eingeben!");
+				}
+
+			} else {
+				System.out.println("Syntax: HashTest [Größe des Arrays][Anzahl der zulesenden Zahlen]");
+			}
+
+		} catch (NumberFormatException e) {
+			System.out.println("Eingabe Falsch! Bitte korrigieren.");
+		}
+
 	}
-	
+
 }
