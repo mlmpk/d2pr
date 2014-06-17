91d6c52c722eece4b83dc848a883c80d4125c424
mlmpk <mlempik@gmail.com>
2014-06-17 09:26:58 +0200

    blatt10 added

---
+++ src/de/lempikbubar/src/blatt10/HashItem.java
@@ -0,0 +1,30 @@
+package de.lempikbubar.src.blatt10;
+
+public class HashItem {
+
+	private String key;
+	private int info;
+
+	public HashItem(String key, int info) {
+		super();
+		this.key = key;
+		this.info = info;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public int getInfo() {
+		return info;
+	}
+
+	public void setInfo(int info) {
+		this.info = info;
+	}
+
+}
---
+++ src/de/lempikbubar/src/blatt10/HashTable.java
@@ -0,0 +1,29 @@
+package de.lempikbubar.src.blatt10;
+
+import java.util.LinkedList;
+
+public class HashTable {
+
+	public static LinkedList<HashItem> hashitemList;
+	
+	public HashTable() {
+		hashitemList = new LinkedList<>();
+	}
+
+	public void put(String key) {
+	}
+
+	public HashItem get(String key) {
+
+		return null;
+	}
+
+	public void clear() {
+	}
+
+	public int numberOfCollisions() {
+		return 0;
+	}
+	
+	public void printHashTable(){}
+}
